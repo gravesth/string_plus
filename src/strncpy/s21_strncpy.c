c04ab7e1dac92c23bc81447cf9713c5f455f7014
Cetheres Alexis <cetherea@student.21-school.ru>
2026-06-04 15:49:39 +0300

    123

--- src/strncpy/s21_strncpy.c
+++ src/strncpy/s21_strncpy.c
@@ -1,6 +1,6 @@
 #include "../s21_string.h"
 
-char *strncpy(char *dest, const char *src, s21_size_t n) {
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
   s21_size_t i = 0;
   for (; i < n && src[i] != '\0'; i++) {
     dest[i] = src[i];
